f7089c569a37e1d8ae6cec7e2e8f1d006af06a65
Josue Medina Guerrero <josuemedina.032810@gmail.com>
2024-12-10 00:33:08 -0500

    generate token

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using api.Dtos.Account;
+using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,11 @@ namespace api.Controllers
     [ApiController]
     public class AccountController : ControllerBase {
         private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
 
-        public AccountController(UserManager<AppUser> userManager) {
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService) {
             _userManager = userManager;
+            _tokenService = tokenService;
         }
 
         [HttpPost]
@@ -29,7 +32,12 @@ namespace api.Controllers
                 if (createUser.Succeeded) {
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
 
-                    if (roleResult.Succeeded) return Ok("User created");
+                    if (roleResult.Succeeded) return Ok(new NewUserDto {
+                        UserName = appUser.UserName,
+                        Email = appUser.Email,
+                        Token = _tokenService.CreateToken(appUser)
+                    });
+
                     return StatusCode(500, roleResult.Errors);
                 } else {
                     return StatusCode(500, createUser.Errors);
--- Dtos/Account/RegisterDto.cs
+++ Dtos/Account/RegisterDto.cs
@@ -4,13 +4,13 @@ namespace api.Dtos.Account
 {
     public class RegisterDto {
         [Required]
-        public string? Username { get; set; }
+        public string Username { get; set; } = string.Empty;
         
         [Required]
         [EmailAddress]
-        public string? Email { get; set; }
+        public string Email { get; set; } = string.Empty;
         
         [Required]
-        public string? Password { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
